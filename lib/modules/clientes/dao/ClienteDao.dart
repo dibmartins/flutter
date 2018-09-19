3d28008d2a2f8b78e5df1728466aa52fed62b2a3
Diego Botelho <dibmartins@gmail.com>
2018-09-19 07:36:29 -0300

    inserindo e listando registros no bd

---
+++ lib/modules/clientes/dao/ClienteDao.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:forca_vendas/modules/app/database/DbHelper.dart';
+import 'package:forca_vendas/modules/clientes/models/Cliente.dart';
+
+class ClienteDao {
+    
+    final DbHelper database = new DbHelper();
+
+    Database _connection;
+
+    Database get connection => _connection;
+    
+    final String sourceTable = 'clientes';
+    final String idCliente   = 'id_cliente';
+    final String nome        = 'title';
+    final String telefone    = 'telefone';
+    final String email       = 'email';
+
+    Future<int> save(Cliente note) async {
+
+        _connection = await database.db;
+        
+        return await connection.insert(sourceTable, note.toMap());
+    }
+    
+    Future<int> update(Cliente note) async {
+
+        _connection = await database.db;
+        
+        return await connection.update(sourceTable, note.toMap(), where: "$idCliente = ?", whereArgs: [note.id]);
+    }
+
+    Future<int> delete(int id) async {
+
+        _connection = await database.db;
+        
+        return await connection.delete(sourceTable, where: '$idCliente = ?', whereArgs: [id]);
+    }
+    
+    Future<List> fetch(List<String> columns) async {
+
+        _connection = await database.db;
+        
+        var result = await connection.query(sourceTable, columns: columns);
+    
+        return result.toList();
+    }
+    
+    Future<Cliente> findFirst(int id, List<String> columns) async {
+
+        _connection = await database.db;
+        
+        List<Map> result = await connection.query(
+            sourceTable,
+            columns: columns,
+            where: '$idCliente = ?',
+            whereArgs: [id]
+        );
+    
+        if (result.length <= 0) return null;
+            
+        return new Cliente.fromMap(result.first);
+    }
+
+    Future<int> count() async {
+
+        _connection = await database.db;
+        
+        return Sqflite.firstIntValue(await connection.rawQuery('SELECT COUNT(*) FROM $sourceTable'));
+    }
+    
+    Future close() async {
+
+        _connection = await database.db;
+        
+        return connection.close();
+    }
+}
